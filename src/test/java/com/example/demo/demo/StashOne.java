0206294e11f59bd8d8f22a2723663f551a8d7308
yangjianzhen <844771524@qq.com>
2023-12-10 18:43:49 +0900

    stash commit

---
+++ src/test/java/com/example/demo/demo/StashOne.java
@@ -0,0 +1,10 @@
+package com.example.demo.demo;
+
+public class StashOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("stash2");
+	}
+
+}
