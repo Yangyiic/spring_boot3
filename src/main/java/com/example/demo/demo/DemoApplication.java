bf9e2add313d16218ded7f4d4ea42fcfa64837d8
yangjianzhen <844771524@qq.com>
2023-12-12 22:49:18 +0900

    devlop  003

--- src/main/java/com/example/demo/demo/DemoApplication.java
+++ src/main/java/com/example/demo/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("devlop  002");
+		System.out.println("devlop  003");
 	}
 
 }
