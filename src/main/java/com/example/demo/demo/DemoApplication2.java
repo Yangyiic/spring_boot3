6657bfc1132fe19b216fa30231951dce0e2055d9
yangjianzhen <844771524@qq.com>
2023-12-12 22:48:42 +0900

    devlop oo2

---
+++ src/main/java/com/example/demo/demo/DemoApplication2.java
@@ -0,0 +1,10 @@
+package com.example.demo.demo;
+
+public class DemoApplication2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("devlop  002");
+	}
+
+}
